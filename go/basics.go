89fd07d7842b6fbbc15391e2437b227ad1dbcee2
Justin Bankes <jbankes@mail.csuchico.edu>
2018-02-10 09:32:10 -0800

    Work with basic git commands, naked returns, vars, and funcs

--- go/basics.go
+++ go/basics.go
@@ -3,18 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var name string
+	name := Get_User_Info()
 	prompt := "Hello" 
+	Greeting(name, prompt)
+	Goodbye(name)
+}
+
+func Get_User_Info() (name string) {
+	fmt.Printf("User's Name: ");
 	fmt.Scan(&name)
-	defer Goodbye(name)
-	Greeting(prompt, name)
+	return 
 }
 
 // Print Salutation to User
 func Greeting(name string, prompt string) {
-  fmt.Printf("%s, %s\n", name, prompt)
+	fmt.Printf("%s, %s\n", prompt, name)
 }
 
 func Goodbye(name string) {
 	fmt.Printf("Goodbye, %s\n", name)
+	fmt.Println("Try this again soon!")
 }
---
+++ go/go_basics.md
@@ -0,0 +1,27 @@
+# Go Basics
+
+A list of Go Basics to cover
+
+## Topics
+
+### Types
+- [x] Basic
+- [ ] Pointers
+- [ ] Enum
+- [ ] Slices vs Arrays
+
+### Input/Output
+- [ ] Command line I/O
+  - [ ] Printf
+  - [ ] Println
+  - [ ] Scanf
+- [ ] File I/O
+
+### Functions
+- [x] Basic
+- [x] Naked Returns
+- [ ] Multiple Returns
+
+### Classes
+- [ ] Class Creation
+- [ ] Inheritance
