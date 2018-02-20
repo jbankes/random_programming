739d0bc126bdbe38a7cf23d08ebc70ec80476d7b
Justin Bankes <jbankes1@gmail.com>
2018-02-20 15:39:49 -0800

    Installs Bamboo

--- bamboo/setup_env.sh
+++ bamboo/setup_env.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Maven Vars
 MVN_VER="3.5.2"
 MVN_DIR=apache-maven-$MVN_VER
 MVN_TAR=$MVN_DIR-bin.tar.gz
+
+# Bamboo Vars
+BAMBOO_VER="6.3.2"
+BAMBOO_DIR=atlassian-bamboo-$BAMBOO_VER
+BAMBOO_TAR=$BAMBOO_DIR.tar.gz
+BAMBOO_CONFIG=/usr/bin/$BAMBOO_DIR/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties
+BAMBOO_INSTALL=/usr/bin/$BAMBOO_DIR
 
 # install Java
 sudo yum install java-1.8.0-openjdk-devel -y 
@@ -20,5 +28,16 @@ chown -R root:root /opt/$MVN_DIR
 ln -s /opt/$MVN_DIR /opt/apache-maven
 echo 'export PATH=$PATH:/opt/apache-maven/bin' | sudo tee -a /etc/profile
 
+# install bamboo
+if [ ! -d "$BAMBOO_DIR" ]; then
+  wget https://www.atlassian.com/software/bamboo/downloads/binary/$BAMBOO_TAR
+  tar -zxvf $BAMBOO_TAR
+  rm $BAMBOO_TAR
+fi
 
+mv /home/vagrant/$BAMBOO_DIR $BAMBOO_INSTALL
+mkdir /var/lib/bamboo-home
+echo "bamboo.home=/var/lib/bamboo-home" > $BAMBOO_CONFIG
+chown -R vagrant:vagrant $BAMBOO_CONFIG
+chown -R vagrant:vagrant $BAMBOO_INSTALL
 
