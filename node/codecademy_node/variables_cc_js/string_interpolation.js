96ae331da09b34d2bb387a2f7a78732416f44759
Justin Bankes <you@example.com>
2018-06-05 23:37:09 -0700

    add string interpolation and math operators

---
+++ node/codecademy_node/variables_cc_js/string_interpolation.js
@@ -0,0 +1,7 @@
+let favoriteAnimal = 'dog';
+console.log('My favorite animal: ' + favoriteAnimal);
+
+// ES6 style
+let myName = 'Justin';
+let myCity = 'Chico';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
